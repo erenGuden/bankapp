8fae360c6911508b7fe4fa570d43a9e1c43e01e5
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-25 15:23:46 -0700

    Create routes
    Create routes for "/home", "/login", "/signup"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Signup from "./components/Signup";
-
+import Login from "./components/Login";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
 function App() {
-  return <Signup />;
+  return (
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;

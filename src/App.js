2f30122c30e3fc51fb63be3c9c23030fe68b4efa
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-26 18:23:07 -0700

    Implement "/login"
    Add login page and style it with MUI.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Signup from "./components/Signup";
 import Login from "./components/Login";
-import Home from "./components/Home";
-import { Route, Routes } from "react-router-dom";
+import {  Grid } from "@mui/material";
+
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <Grid>
+      <Login />
+    </Grid>
   );
 }
 

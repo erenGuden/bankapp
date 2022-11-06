a525deeee7da6ea621adb8608c9c77effb0a9331
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-11-05 22:02:55 -0700

    Sign up imlementation
    Add "/signup" page and logic implementation.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import {
   Button,
   Divider,
   Grid,
+  Link,
   Stack,
   TextField,
   Typography,
@@ -23,12 +24,15 @@ const GridStyled = styled(Grid)(({ theme }) => ({
   },
 }));
 
+// /api/users?username=
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const navigate = useNavigate();
-  const baseurl = `http://localhost:3000/api/auth`
+  const baseurl = `http://localhost:3000/api/auth`;
+
   const submitHandler = (e) => {
     e.preventDefault();
 
@@ -132,9 +136,14 @@ const Login = () => {
                 >
                   Login
                 </Button>
-                <Typography variant="subtitle2" padding="25px">
+                <Link
+                  href="/signup"
+                  color="inherit"
+                  variant="subtitle2"
+                  padding="25px"
+                >
                   Don't have an account?
-                </Typography>
+                </Link>
               </Stack>
             </form>
           </Grid>

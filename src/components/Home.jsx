571e39d008dab7c81075ee46099d8130ff63aade
Eren Guden <105981672+erenGuden@users.noreply.github.com>
2022-12-16 12:16:24 -0800

    Create new account (#20)
    * Add "Create new account" function
    
    - Implement logic,
    - Add styling
    
    * Add box styling
    
    Add box styling
    
    * Fixed TextField
    
    Move BoxStyled out of the Home function and add export in order to fix it.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,62 @@
+import {
+  Box,
+  Divider,
+  Grid,
+  Modal, Stack, TextField
+} from "@mui/material";
 import * as React from "react";
 import AccountCard from "./AccountCard";
 import Button from "@mui/material/Button";
-import Navbar from "./Navbar";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
-import { Divider, Grid } from "@mui/material";
-import { Stack } from "@mui/material";
-import { useEffect } from "react";
-import { useState } from "react";
+import { styled } from "@mui/system";
+import { useEffect, useState } from "react";
+
+export const BoxStyled = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    width: "90vw",
+    height: "18vh",
+  },
+}));
 
 const Home = () => {
-  const id = localStorage.getItem("id");
-  const baseurl = `http://localhost:3000/api/accounts?userId=${id}`;
+  const userId = localStorage.getItem("id");
+  const baseurl = `http://localhost:3000/api/accounts?userId=${userId}`;
+  const createAccountUrl = `http://localhost:3000/api/accounts`;
   const [accounts, setAccounts] = useState([]);
   const [userDetails, setUserDetails] = useState([]);
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(false);
+  const handleClose = () => setOpen(!open);
+
+  const ButtonStyled = styled(Button)(({ theme }) => ({
+    width: "10vw",
+    marginLeft: "115px",
+    marginBottom: "10px",
+    backgroundColor: "lightblue",
+    [theme.breakpoints.down("sm")]: {
+      width: "30vw",
+      marginLeft: "35px",
+    },
+  }));
+
+  const handleSubmit = (e) => {
+    axios
+      .post(createAccountUrl, { name, userId })
+      .then((response) => {
+        window.location.reload(true);
+      })
+      .catch((error) => {
+        setError(true);
+        setTimeout(() => {
+          setError(false);
+        }, 4000);
+      });
+  };
 
   // Get user details with userID
-  const userDetailsUrl = `http://localhost:3000/api/users/${id}`;
+  const userDetailsUrl = `http://localhost:3000/api/users/${userId}`;
 
   useEffect(() => {
     axios.get(baseurl).then((response) => {
@@ -28,7 +68,6 @@ const Home = () => {
       setUserDetails(data);
     });
   }, []);
-
 
   // Capitalize the first letter of username
   const capitalizeFirstLetter = (word) => {
@@ -59,6 +98,7 @@ const Home = () => {
             {accounts.map((account) => (
               <AccountCard account={account} key={account._id} />
             ))}
+
             <Button
               color="primary"
               variant="contained"
@@ -67,9 +107,62 @@ const Home = () => {
                 width: "45vh",
               }}
               type="submit"
+              onClick={handleClose}
             >
-              Create new account
+              Create New Account
             </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <BoxStyled
+                sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: "20vw",
+                  height: "16vh",
+                  backgroundColor: "white",
+                  border: "4px solid skyblue",
+                  display: "inline",
+                  paddingTop: "14px",
+                }}
+              >
+                {error && (
+                  <Typography align="center" color="red" fontWeight="bold">
+                    Account name can not be empty
+                  </Typography>
+                )}
+                <TextField
+                  label="Account name"
+                  variant="standard"
+                  required
+                  sx={{
+                    width: "80%",
+                    margin: "18px",
+                    marginLeft: "30px",
+                  }}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <ButtonStyled
+                  color="primary"
+                  variant="contained"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </ButtonStyled>
+                <ButtonStyled
+                  color="primary"
+                  variant="contained"
+                  onClick={handleClose}
+                >
+                  Cancel
+                </ButtonStyled>
+              </BoxStyled>
+            </Modal>
           </Stack>
         </Grid>
       )}
